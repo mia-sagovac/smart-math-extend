75934bc6da6c4b183af4aa5bcf0089304e75bff1
mia-sagovac <mia.sagovac@fer.hr>
2026-04-28 08:14:33 +0200

    pool size

--- backend/app/db.py
+++ backend/app/db.py
@@ -2,7 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from .config import settings
 
-engine = create_engine(settings.DATABASE_URL, echo=True, future=True)
+engine = create_engine(
+    settings.DATABASE_URL,
+    pool_size=20,
+    max_overflow=30,
+    pool_timeout=30
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
